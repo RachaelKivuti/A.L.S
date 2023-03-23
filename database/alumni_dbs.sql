0943dcb86bfc18164c3cd481996cfd043856e261
RachaelKivuti <kivutingatha@gmail.com>
2023-03-23 23:25:37 +0300

    mysql database

---
+++ database/alumni_dbs.sql
@@ -0,0 +1,399 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 15, 2022 at 07:15 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
+
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `alumni_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `alumnus_bio`
+--
+
+CREATE TABLE `alumnus_bio` (
+  `id` int(30) NOT NULL,
+  `firstname` varchar(200) NOT NULL,
+  `middlename` varchar(200) NOT NULL,
+  `lastname` varchar(200) NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `batch` year(4) NOT NULL,
+  `course_id` int(30) NOT NULL,
+  `email` varchar(250) NOT NULL,
+  `connected_to` text NOT NULL,
+  `avatar` text NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 0 COMMENT '0= Unverified, 1= Verified',
+  `date_created` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `alumnus_bio`
+--
+
+INSERT INTO `alumnus_bio` (`id`, `firstname`, `middlename`, `lastname`, `gender`, `batch`, `course_id`, `email`, `connected_to`, `avatar`, `status`, `date_created`) VALUES
+(4, 'Fredrick', 'Maore', 'Kimathi', 'Male', 2020, 1, 'fredrickkimathi26@gmail.com', '', '1662368760_WhatsApp Image 2022-09-05 at 11.55.32 AM.jpeg', 1, '2022-09-05'),
+(5, 'Racheal', 'Ngatha', 'Kivuti', 'Female', 2020, 3, 'rachealngatha254@gmail.com', 'Arcane', '1662397800_racheal.jpeg', 1, '2022-09-05'),
+(6, 'Angel', '', 'Praise', 'Female', 2020, 4, 'angelpraise@gmail.com', 'Angel', '1662397680_praise.jpeg', 1, '2022-09-05'),
+(7, 'githu', 'maina', 'kelvin', 'Male', 2024, 3, 'kel@gmail.com', 'fdt', '', 1, '2022-09-06'),
+(8, 'Ashley', 'Amaani', 'Owaya', 'Female', 2020, 3, 'ashamaani@gmail.com', 'data scientist', '1662448080_WhatsApp Image 2022-09-06 at 9.32.48 AM.jpeg', 1, '2022-09-06'),
+(9, 'Jayson', 'Alfie', 'Abwoya', 'Male', 2019, 3, 'alfie@gmail.com', 'TUK', '1662448740_WhatsApp Image 2022-09-06 at 10.15.20 AM.jpeg', 1, '2022-09-06'),
+(10, 'Annex', 'A', 'Gitonga ', 'Male', 2018, 3, 'annex@gmail.com', 'Annex Co', '', 1, '2022-09-07');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `careers`
+--
+
+CREATE TABLE `careers` (
+  `id` int(30) NOT NULL,
+  `company` varchar(250) NOT NULL,
+  `location` text NOT NULL,
+  `job_title` text NOT NULL,
+  `description` text NOT NULL,
+  `user_id` int(30) NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `careers`
+--
+
+INSERT INTO `careers` (`id`, `company`, `location`, `job_title`, `description`, `user_id`, `date_created`) VALUES
+(1, 'IT Company', 'Home-Based', 'Web Developer', '&lt;p style=&quot;-webkit-tap-highlight-color: rgba(0, 0, 0, 0); margin-top: 1.5em; margin-bottom: 1.5em; line-height: 1.5; animation: 1000ms linear 0s 1 normal none running fadeInLorem;&quot;&gt;Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sagittis eu volutpat odio facilisis mauris sit amet massa vitae. In tellus integer feugiat scelerisque varius morbi enim. Orci eu lobortis elementum nibh tellus molestie nunc. Vulputate ut pharetra sit amet aliquam id diam maecenas ultricies. Lacus sed viverra tellus in hac habitasse platea dictumst vestibulum. Eleifend donec pretium vulputate sapien nec. Enim praesent elementum facilisis leo vel fringilla est ullamcorper. Quam adipiscing vitae proin sagittis nisl rhoncus. Sed viverra ipsum nunc aliquet bibendum. Enim ut sem viverra aliquet eget sit amet tellus. Integer feugiat scelerisque varius morbi enim nunc faucibus.&lt;/p&gt;&lt;p style=&quot;-webkit-tap-highlight-color: rgba(0, 0, 0, 0); margin-top: 1.5em; margin-bottom: 1.5em; line-height: 1.5; animation: 1000ms linear 0s 1 normal none running fadeInLorem;&quot;&gt;Viverra justo nec ultrices dui. Leo vel orci porta non pulvinar neque laoreet. Id semper risus in hendrerit gravida rutrum quisque non tellus. Sit amet consectetur adipiscing elit ut. Id neque aliquam vestibulum morbi blandit cursus risus. Tristique senectus et netus et malesuada. Amet aliquam id diam maecenas ultricies mi eget mauris. Morbi tristique senectus et netus et malesuada. Diam phasellus vestibulum lorem sed risus. Tempor orci dapibus ultrices in. Mi sit amet mauris commodo quis imperdiet. Quisque sagittis purus sit amet volutpat. Vehicula ipsum a arcu cursus. Ornare quam viverra orci sagittis eu volutpat odio facilisis. Id volutpat lacus laoreet non curabitur. Cursus euismod quis viverra nibh cras pulvinar mattis nunc. Id aliquet lectus proin nibh nisl condimentum id venenatis. Eget nulla facilisi etiam dignissim diam quis enim lobortis. Lacus suspendisse faucibus interdum posuere lorem ipsum dolor sit amet.&lt;/p&gt;', 3, '2020-10-15 14:14:27'),
+(2, 'Arcane Electronics Limited', 'The Technical University of Kenya', 'IT Specialist', '&lt;p style=&quot;margin-top: 1.5em; margin-bottom: 1.5em; margin-right: unset; margin-left: unset; color: rgb(68, 68, 68); font-family: &amp;quot;Open Sans&amp;quot;, sans-serif; font-size: 16px; -webkit-tap-highlight-color: rgba(0, 0, 0, 0); line-height: 1.5; animation: 1000ms linear 0s 1 normal none running fadeInLorem;&quot;&gt;Arcane Electronic Dealers is looking for a certified IT specialist with common knowledge in database management and data manipulation. if you fully meet these qualifications, contact Arcane Limited for the interview or go to their location in TUK, room D21&lt;/p&gt;', 1, '2020-10-15 15:05:37'),
+(4, 'racheal', 'wilson', 'pilot', 'looking for a pilot that has 4 years experience and ready to work at odd hours', 6, '2022-09-06 08:45:57'),
+(5, 'Angel', 'pioneer', 'journalist', 'looking for a journalist with 2 years experience to work for a 6 year contract', 7, '2022-09-06 09:04:49'),
+(6, 'Annex Co', 'New York', 'Hacker', 'i need a hacker who can work perfectly under pressure', 11, '2022-09-07 11:37:47');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `courses`
+--
+
+CREATE TABLE `courses` (
+  `id` int(30) NOT NULL,
+  `course` text NOT NULL,
+  `about` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `courses`
+--
+
+INSERT INTO `courses` (`id`, `course`, `about`) VALUES
+(1, 'Btech Information Technology', 'Sample'),
+(3, 'Btech Computer Technology', ''),
+(4, 'Btech Computer Networks', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `events`
+--
+
+CREATE TABLE `events` (
+  `id` int(30) NOT NULL,
+  `title` varchar(250) NOT NULL,
+  `content` text NOT NULL,
+  `schedule` datetime NOT NULL,
+  `banner` text NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `events`
+--
+
+INSERT INTO `events` (`id`, `title`, `content`, `schedule`, `banner`, `date_created`) VALUES
+(2, 'Mr and Mrs TUK', 'We are obligated to welcome you for the Technical University of Kenya Mr and Mrs TUK event.&lt;p&gt;feel free to attend, there are no charges for all alumni members.&lt;/p&gt;', '2022-09-30 19:00:00', '', '2022-09-07 17:47:44'),
+(3, 'games ', 'you are all welcome to cheer your best TUK teams and be a part of a team that you were once part of', '2022-09-22 10:00:00', '', '2022-09-07 18:02:44');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `event_commits`
+--
+
+CREATE TABLE `event_commits` (
+  `id` int(30) NOT NULL,
+  `event_id` int(30) NOT NULL,
+  `user_id` int(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `event_commits`
+--
+
+INSERT INTO `event_commits` (`id`, `event_id`, `user_id`) VALUES
+(1, 1, 3),
+(2, 2, 5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `forum_comments`
+--
+
+CREATE TABLE `forum_comments` (
+  `id` int(30) NOT NULL,
+  `topic_id` int(30) NOT NULL,
+  `comment` text NOT NULL,
+  `user_id` int(30) NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `forum_comments`
+--
+
+INSERT INTO `forum_comments` (`id`, `topic_id`, `comment`, `user_id`, `date_created`) VALUES
+(1, 3, 'Sample updated Comment', 3, '2020-10-15 15:46:03'),
+(3, 3, 'Sample', 1, '2020-10-16 08:48:02'),
+(5, 0, '', 1, '2020-10-16 09:49:34'),
+(6, 1, 'i will be attending&amp;nbsp;', 5, '2022-09-07 17:56:53');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `forum_topics`
+--
+
+CREATE TABLE `forum_topics` (
+  `id` int(30) NOT NULL,
+  `title` varchar(250) NOT NULL,
+  `description` text NOT NULL,
+  `user_id` int(30) NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `forum_topics`
+--
+
+INSERT INTO `forum_topics` (`id`, `title`, `description`, `user_id`, `date_created`) VALUES
+(2, 'Sample Topic 2', '&lt;span style=&quot;color: rgb(0, 0, 0); font-family: &amp;quot;Open Sans&amp;quot;, Arial, sans-serif; text-align: justify;&quot;&gt;&quot;Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.&quot;&lt;/span&gt;', 3, '2020-10-15 15:20:51'),
+(3, 'Sample Topic 3', '&lt;span style=&quot;color: rgb(0, 0, 0); font-family: &amp;quot;Open Sans&amp;quot;, Arial, sans-serif; text-align: justify;&quot;&gt;Vivamus gravida nunc orci. Proin ut tristique odio. Nulla suscipit ipsum arcu, a luctus lorem vulputate et. Maecenas magna lorem, tempor id ultrices id, vehicula eu diam. Aliquam erat volutpat. Praesent in sem tincidunt, mattis odio nec, ultrices justo. Vivamus sit amet sapien ornare tortor porttitor congue vel et lorem. In interdum eget metus ut sagittis. In accumsan nec purus vel ornare. Quisque non scelerisque libero, et aliquam risus. Mauris tincidunt ullamcorper efficitur. Nullam venenatis in massa et elementum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; In mollis, tortor sed pellentesque ultrices, sem sem interdum lectus, a laoreet nulla lacus at risus. Ut placerat orci at enim fermentum, eget pretium ante pharetra. Nam id nunc congue augue feugiat egestas.&lt;/span&gt;', 3, '2020-10-15 15:22:30'),
+(5, 'Cultural Week', 'You are welcome for Mr and Mrs TUK event that will take place from 30th September. Entry is free for all alumni.', 1, '2022-09-07 17:53:43');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gallery`
+--
+
+CREATE TABLE `gallery` (
+  `id` int(30) NOT NULL,
+  `about` text NOT NULL,
+  `created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `gallery`
+--
+
+INSERT INTO `gallery` (`id`, `about`, `created`) VALUES
+(6, 'ADMINSTRATION BLOCK', '2022-09-15 07:29:52');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `system_settings`
+--
+
+CREATE TABLE `system_settings` (
+  `id` int(30) NOT NULL,
+  `name` text NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `contact` varchar(20) NOT NULL,
+  `cover_img` text NOT NULL,
+  `about_content` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `system_settings`
+--
+
+INSERT INTO `system_settings` (`id`, `name`, `email`, `contact`, `cover_img`, `about_content`) VALUES
+(1, 'Alumni Management System', 'group2@gmail.com', '+254746151124', '1662369960_tuk.jpg', '&lt;p style=&quot;text-align: center; background: transparent; position: relative;&quot;&gt;This system is as a result of group work by Fredrick Kimathi, Racheal Ngatha and Angel Praise. The system creates a virtual platform where the Admin, in this case which is the Technical University of Kenya can easily communicate with the Alumni from the institution. The software makes it possible to pass information such as jobs and upcoming events.&amp;nbsp;&lt;/p&gt;&lt;p&gt;&lt;/p&gt;');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(30) NOT NULL,
+  `name` text NOT NULL,
+  `username` varchar(200) NOT NULL,
+  `password` text NOT NULL,
+  `type` tinyint(1) NOT NULL DEFAULT 3 COMMENT '1=Admin,2=Alumni officer, 3= alumnus',
+  `auto_generated_pass` text NOT NULL,
+  `alumnus_id` int(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `username`, `password`, `type`, `auto_generated_pass`, `alumnus_id`) VALUES
+(1, 'Admin', 'admin', '0192023a7bbd73250516f069df18b500', 1, '', 0),
+(5, 'Fredrick Kimathi', 'fredrickkimathi26@gmail.com', '6a536a4409be00b1f61248b8b0b8af5a', 3, '', 4),
+(6, 'Racheal Kivuti', 'rachealngatha254@gmail.com', '46ca098208c55cae0608d265ce4bc3d8', 3, '', 5),
+(7, 'Angel Praise', 'angelpraise@gmail.com', '25730bb73e17447550dcb468a66ea5cd', 3, '', 6),
+(8, 'githu kelvin', 'kel@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055', 3, '', 7),
+(9, 'Ashley Owaya', 'ashamaani@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055', 3, '', 8),
+(10, 'Jayson Abwoya', 'alfie@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055', 3, '', 9),
+(11, 'Annex Gitonga ', 'annex@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055', 3, '', 10);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `alumnus_bio`
+--
+ALTER TABLE `alumnus_bio`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `careers`
+--
+ALTER TABLE `careers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `courses`
+--
+ALTER TABLE `courses`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `events`
+--
+ALTER TABLE `events`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `event_commits`
+--
+ALTER TABLE `event_commits`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `forum_comments`
+--
+ALTER TABLE `forum_comments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `forum_topics`
+--
+ALTER TABLE `forum_topics`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `gallery`
+--
+ALTER TABLE `gallery`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `system_settings`
+--
+ALTER TABLE `system_settings`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `alumnus_bio`
+--
+ALTER TABLE `alumnus_bio`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `careers`
+--
+ALTER TABLE `careers`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `courses`
+--
+ALTER TABLE `courses`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `events`
+--
+ALTER TABLE `events`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `event_commits`
+--
+ALTER TABLE `event_commits`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `forum_comments`
+--
+ALTER TABLE `forum_comments`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `forum_topics`
+--
+ALTER TABLE `forum_topics`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `gallery`
+--
+ALTER TABLE `gallery`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `system_settings`
+--
+ALTER TABLE `system_settings`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
